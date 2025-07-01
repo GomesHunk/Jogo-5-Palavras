09ce0e2d69e712f2f005d1e7394cc4106ee9d6c7
GomesHunk <110572451+GomesHunk@users.noreply.github.com>
2025-06-30 23:50:36 -0300

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'jogo_das_palavras_secre
 socketio = SocketIO(
     app, 
     cors_allowed_origins="*",
-    async_mode='eventlet',
+    async_mode='gevent',
     ping_timeout=60,
     ping_interval=25,
     logger=False,
